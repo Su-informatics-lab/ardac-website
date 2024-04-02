ab3124b2fa67e3aeceff74ad7704cd4cf486fbed
Alan Walsh <alwalsh@iu.edu>
2024-04-02 13:35:03 -0400

    Terraform files
    Initial version of Terraform deployment scripts.

---
+++ terraform/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "s3" {
+    
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.42.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+}
